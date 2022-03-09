4215d528f9ed6b37b2ec0dadf29724fa03d40270
Jiaojiao.Ding72 <jiaojiaon@gmail.com>
2022-03-08 21:47:14 -0500

    upload part three of lab

--- SQLscripts.sql
+++ SQLscripts.sql
@@ -48,3 +48,57 @@ INSERT INTO Rank(Ranking, Car_ID,Year,Car_Make,Car_Model) SELECT rowid, Car_ID,C
 .mode csv
 .output extract1.csv
 SELECT * FROM Rank;
+
+
+ CREATE TABLE RankWithTotal(
+   ...> Rank int,
+   ...> Car_Make text,
+   ...> Car_ID int,
+   ...> Total_Score int);
+
+INSERT INTO RankWithTotal(Rank, Car_Make,Car_ID,Total_Score) SELECT Rank.Ranking,Rank.Car_Make,Rank.Car_ID,Car_Rank.Car_Total FROM Rank,Car_Rank WHERE Rank.Car_ID = Car_Rank.Car_ID;
+
+CREATE TABLE RankTopOne(
+   ...> Rank int,
+   ...> Car_Make text,
+   ...> Car_ID int,
+   ...> Total_Score int);
+
+INSERT INTO RankTopOne(Rank, Car_Make,Car_ID,Total_Score)SELECT Rank, Car_Make,Car_ID,Total_Score FROM RankWithTotal GROUP BY Car_Make;
+
+DELETE FROM RankWithTotal WHERE EXISTS (SELECT * FROM RankTopOne WHERE RankTopOne.Rank == RankWithTotal.Rank);
+
+CREATE TABLE RankTopTwo(
+   ...> Rank int,
+   ...> Car_Make text,
+   ...> Car_ID int,
+   ...> Total_Score int);
+
+INSERT INTO RankTopTwo(Rank, Car_Make,Car_ID,Total_Score)SELECT MIN(Rank), Car_Make,Car_ID,Total_Score FROM RankWithTotal GROUP BY Car_Make ORDER BY Rank;
+ 
+DELETE FROM RankWithTotal WHERE EXISTS (SELECT * FROM RankTopTwo WHERE RankTopTwo.Rank == RankWithTotal.Rank);
+
+CREATE TABLE RankTopThree(
+   ...> Rank int,
+   ...> Car_Make text,
+   ...> Car_ID int,
+   ...> Total_Score int);
+
+INSERT INTO RankTopThree(Rank, Car_Make,Car_ID,Total_Score)SELECT MIN(Rank), Car_Make,Car_ID,Total_Score FROM RankWithTotal GROUP BY Car_Make ORDER BY Rank;
+
+
+CREATE TABLE TopThree_Car(
+   ...> Rank int,
+   ...> Car_Make text,
+   ...> Car_ID int,
+   ...> Total_Score int);
+
+INSERT INTO TopThree_Car(Rank, Car_Make,Car_ID,Total_Score) SELECT * FROM RankTopOne UNION SELECT * FROM RankTopTwo UNION SELECT * FROM RankTopThree ORDER BY Car_Make, rank;
+
+.mode csv
+.output extract2.csv
+SELECT * FROM TopThree_Car;
+
+
+
+
--- script.awk
+++ script.awk
@@ -2,7 +2,7 @@ BEGIN {
 FS = ","
 OFS = ","
 print "Car_ID, Year, Car_Make, Car_Model, Owner_Name, Owner_Email" > "carsTable.csv"
-print "Judge_ID, Judge_Name" > "judgesTable.csv"
+print "Timestamp,Judge_ID, Judge_Name" > "judgesTable.csv"
 print "Car_ID, Car_Score" > "Car_Score.csv"
 }
 {
@@ -12,7 +12,7 @@ for(i=10; i<= NF;i++){
 total+=$i
 }
 print $7, total  > "Car_Score.csv"
-print  $8, $9  > "judgesTable.csv"
+print $1, $8, $9  > "judgesTable.csv"
 print $7, $4, $5, $6, $3, $2  > "carsTable.csv"
 }
 }
